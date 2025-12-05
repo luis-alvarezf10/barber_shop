30c48d1e3b9d5ebfd65b9346732d127a06eeffe9
Luis Álvarez <luigi>
2025-12-05 13:33:04 -0400

    fix: simplificar app.py y agregar migraciones al build

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-import os
-import django
-
-# Configurar Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
-django.setup()
-
-# Ejecutar migraciones automáticamente en el primer request
-from django.core.management import call_command
-from django.db import connection
-from django.db.utils import OperationalError
-
-try:
-    # Verificar si las tablas existen
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT 1 FROM django_migrations LIMIT 1")
-except OperationalError:
-    # Si no existen, ejecutar migraciones
-    print("Ejecutando migraciones...")
-    call_command('migrate', '--noinput')
-    print("Migraciones completadas!")
-
 from application.wsgi import application
 
 # Vercel necesita que la aplicación se llame 'app'
--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,8 @@
 # Instala dependencias
 pip install -r requirements.txt
 
+# Ejecuta migraciones
+python manage.py migrate --noinput
+
 # Recolecta archivos estáticos
 python manage.py collectstatic --noinput --clear
