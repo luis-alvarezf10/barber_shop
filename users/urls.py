0b23a9e947d11164af99492d0361e60722a1cf47
Luis Álvarez <luigi>
2025-12-05 13:48:06 -0400

    fix: eliminada vista de crear super user

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    # TEMPORAL: Eliminar después de crear el primer admin
-    path('setup-admin/', views.create_first_admin, name='setup_admin'),
 ]
